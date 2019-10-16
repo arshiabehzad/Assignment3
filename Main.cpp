16f0463e7ff21a45f6cfba90d1ad8acb85dca328
arshiabehzad <arshia.school1@gmail.com>
2019-10-16 00:51:23 +0000

    option to analyze another file added

--- Main.cpp
+++ Main.cpp
@@ -5,14 +5,24 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    string filename = argv[1];
+    string filename;
+    filename = argv[1];
     FileAnalyzer* f1 = new FileAnalyzer(filename);
-    try{
-        f1->findPairs();
-        cout <<"no errors found" <<endl;
-    }catch(runtime_error e)
+    while (true)
     {
-        cout<<e.what()<<endl;
+        try{
+            f1->findPairs();
+            cout <<"no errors found" <<endl;
+        }catch(runtime_error e)
+        {
+            cout<<e.what()<<endl;
+        }
+        cout << "Enter the file name to analyze another file (enter -1 to exit)" <<endl;
+        cin >> filename;
+        if (filename == "-1")
+        {
+            break;
+        }
+        f1 = new FileAnalyzer(filename);
     }
-    
 }
